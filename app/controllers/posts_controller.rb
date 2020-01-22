4de16fcf31a627287b8d85704e546a230c423c69
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-01-22 22:32:31 +0900

    ルーティング設定
    ルーティング設定。
    postsテーブル
    user_id追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,17 @@ class PostsController < ApplicationController
   def index
   end  
   
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :meals, :description).merge(user_id: current_user.id)
+  end
 end
